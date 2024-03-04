5a8cec6afb8bb6c75d4aef84e27420cddbe60e18
adrianofsoaresti <guihenrique.dev@gmail.com>
2024-03-04 03:44:18 -0300

    Created first page mock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatCard, MatCardContent } from '@angular/material/card';
+import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbar, MatButtonModule, MatIcon, MatCard, MatCardContent, MatFormFieldModule, MatLabel, MatInputModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
